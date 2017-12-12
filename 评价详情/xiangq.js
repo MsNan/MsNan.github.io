cca0d9d23a823038465456cf834e10afb576db07
guotaogithub <418285382@qq.com>
2017-12-12 18:10:42 +0800

    datu

--- 评价详情/xiangq.js
+++ 评价详情/xiangq.js
@@ -2,12 +2,22 @@ $(function(){
 	var imglist = $(".pjnr img");
 	// console.log(imglist);
 	$(".pjnr").on("click","img",function(){
-		$(".full").css("display","block");
+		$(".full").css("visibility","visible");
 		for(var i =0; i<imglist.length; i++){
 			$(".pjnr img:eq("+i+")").attr("src");
 			// console.log($(".pjnr img:eq("+i+")").attr("src"));
-			console.log($(this).attr("src"));
+			console.log($(".pjnr img:eq("+i+")").attr("src"));
 			//功能未完成
 		}
+	});
+	$(".full").click(function(){
+		$(".full").css("visibility","hidden");
 	})
+
+	var mySwiper = new Swiper('.full',{
+	effect : 'fade',
+	fade: {
+	  crossFade: false,
+	}
+	});
 })
