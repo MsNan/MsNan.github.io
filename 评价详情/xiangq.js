0f1a0f45eb62d3601de552d3ee60d90b39b786ac
guotaogithub <418285382@qq.com>
2017-12-14 09:52:39 +0800

    添加新页面

--- 评价详情/xiangq.js
+++ 评价详情/xiangq.js
@@ -1,26 +1,5 @@
 $(function(){
-	var imglist = $(".pjnr img");
-	// console.log(imglist);
-	$(".pjnr").on("click","img",function(){
-		$(".swiper-container").css("visibility","visible");
-		for(var i =0; i<imglist.length; i++){
-			$(".pjnr img:eq("+i+")").attr("src");
-			// console.log($(".pjnr img:eq("+i+")").attr("src"));
-			console.log($(".pjnr img:eq("+i+")").attr("src"));
-			//功能未完成
-		}
-	});
-	$(".swiper-container").click(function(){
-		$(".swiper-container").css("visibility","hidden");
-	})
-
-	var mySwiper = new Swiper ('.swiper-container', {
-	  direction: 'horizontal',
-	  loop: false,
-	  
-	  // 如果需要分页器
-	  pagination: {
-	    el: '.swiper-pagination',
-	  }
+	$(".cl").click(function(){
+		window.location.href="./pingjiaxj.html"; 
 	})
 })
